a7df7f881cca5d1363eebbbc5a6f71848280c1df
lion <allisonkosy@gmail.com>
2019-11-27 06:29:56 +0100

    Updates

---
+++ precache-manifest.b40093b3a710597bd7b432ce963808ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b13d667396784adfd9920c78e01fdb1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48d8c5c73776d1239175",
+    "url": "/static/css/main.f3166284.chunk.css"
+  },
+  {
+    "revision": "03c5f2972862063b7f9b",
+    "url": "/static/js/2.62b73e84.chunk.js"
+  },
+  {
+    "revision": "48d8c5c73776d1239175",
+    "url": "/static/js/main.40ae5e6a.chunk.js"
+  },
+  {
+    "revision": "06af083999a59d14889e",
+    "url": "/static/js/runtime-main.a2de32b1.js"
+  },
+  {
+    "revision": "c29f641720e2d972e8258a6324136e59",
+    "url": "/static/media/gill.c29f6417.svg"
+  },
+  {
+    "revision": "2ae17a3f949eea9086753f5a922640cf",
+    "url": "/static/media/lon.2ae17a3f.svg"
+  }
+]);
